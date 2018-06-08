f415a9714b54b916673e2e01d62df1cb8e321698
eliranlaor <eliran.laor@mail.huji.ac.il>
2018-06-08 16:27:41 +0300

    Var class - coding done

---
+++ src/oop/ex6/main/LineInfo.java
@@ -0,0 +1,7 @@
+package oop.ex6.main;
+
+public class LineInfo {
+
+    
+
+}
--- src/oop/ex6/main/Var.java
+++ src/oop/ex6/main/Var.java
@@ -1,4 +1,39 @@
 package oop.ex6.main;
 
-public class Var {
+public class Var{
+
+    /*magic numbers*/
+    private final int INT_INDEX = 0;
+    private final int DOUBLE_INDEX = 1;
+    private final int BOOLEAN_INDEX = 2;
+    /*magic numbers - end*/
+
+    /*object parameters*/
+    private boolean isInitialized;
+    private int varType;
+    private boolean isFinal;
+    /*object parameters - end*/
+
+    public Var(boolean isInitialized, int varType, boolean isFinal){
+        this.isInitialized = isInitialized;
+        this.varType = varType;
+        this.isFinal = isFinal;
+    }
+
+    public boolean isFinal() {
+        return isFinal;
+    }
+
+    public int getVarType() {
+        return varType;
+    }
+
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+
+    public void setInitialized() {
+        isInitialized = true;
+    }
+
 }
