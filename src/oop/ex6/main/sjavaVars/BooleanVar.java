92d7ee634def0b2dc1de89f9f22699ac1feb5f89
oriyee <ori.yeffet@huji.mail.ac.il>
2018-06-17 13:00:21 +0300

    booleanVar edit

--- src/oop/ex6/main/sjavaVars/BooleanVar.java
+++ src/oop/ex6/main/sjavaVars/BooleanVar.java
@@ -13,16 +13,20 @@ public class BooleanVar {
     private boolean value;
     private String name;
 
-    private static final String NUMBER_REGEX = "";
+    private static final String DOUBLE_REGEX = "";
+    private static final String TRUE = "true";
+    private static final String FALSE = "false";
 
 
     public BooleanVar(String value, String name) throws InitializationException {
         try {
-            Pattern pattern = Pattern.compile(NUMBER_REGEX);
-            if (!pattern.matcher(value).matches()){
+            Pattern pattern = Pattern.compile(DOUBLE_REGEX);
+            if (!(pattern.matcher(value).matches() || (value.equals(FALSE) || value.equals(TRUE)))){
                 throw new InitializationException();
             }
-            this.value = (Integer.parseInt(pattern.matcher(value).group()) != 0 || value.equals("true"));
+            this.value = (pattern.matcher(value).matches()) ?
+                    (Integer.parseInt(pattern.matcher(value).group()) != 0) :
+                    (value.equals(TRUE)) ? true : false;
             this.name = name;
         }
         catch (Exception e) {
