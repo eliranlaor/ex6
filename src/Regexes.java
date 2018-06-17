687dd1745d21795b50487a65dcebb582a74ba3cb
oriyee <ori.yeffet@huji.mail.ac.il>
2018-06-17 13:45:44 +0300

    regex class in

---
+++ src/Regexes.java
@@ -0,0 +1,7 @@
+public class Regexes {
+
+    public static final String INT="int",DOUBLE="double", STRING="String",FINAL="final",
+            BOOLEAN="boolean", CHAR="char", COMMA=",", EQUAL="=", SPACE = "\\s+", RETURN = "return;";
+
+    final static String VAR_DECLERATION_REGEX ="";
+}
